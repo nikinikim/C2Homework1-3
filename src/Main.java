f1363ebfa60214e650ece43275a1988e00de055b
mosunov.nn <colt92@inbox.ru>
2022-11-22 11:55:44 +0500

    Сделано заданик к ДЗ

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Human maksim = new Human("Максим", "Минск", "бренд-менеджер",1988);
         Human anya = new Human("Аня", "Москва","методист образовательных программ" ,1993);
         Human katya = new Human("Катя", "Калининград","продакт-менеджер", 1992);
-        Human artem = new Human("Артем", "Москва","директор по развитию бизнеса", 1995);
+        Human artem = new Human("Артем", "Москва",null, 1995);
         System.out.println(maksim);
         System.out.println(anya);
         System.out.println(katya);
