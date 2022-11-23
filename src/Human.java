6074e07b18aa6ba9697e286d36b333be79f177b1
mosunov.nn <colt92@inbox.ru>
2022-11-23 15:05:00 +0500

    Сделано задание к ДЗ

--- src/Human.java
+++ src/Human.java
@@ -1,16 +1,16 @@
 public class Human {
-    int yearOfBirth;
-    String name;
-    String town;
-    String jobTitle;
+    private int yearOfBirth;
+    public String name;
+    private String town;
+    public String jobTitle;
 
     Human(String name, String town, String jobTitle, int yearOfBirth) {
-        if ("".equals(name) || name == null) {
+        if (name == null || name.isBlank() || name.isEmpty()) {
             this.name = "Информация не указана";
         }else {
             this.name = name;
         }
-        if ("".equals(town) || town == null) {
+        if (town == null || town.isBlank() || town.isEmpty()) {
             this.town = "Информация не указана";
         }else {
             this.town = town;
@@ -20,10 +20,34 @@ public class Human {
         }else{
             this.yearOfBirth = Math.abs(yearOfBirth);
         }
-        if ("".equals(jobTitle) || jobTitle == null) {
+        if (jobTitle == null || jobTitle.isBlank() || jobTitle.isEmpty()) {
             this.jobTitle = "Информация не указана";
         }else {
             this.jobTitle = jobTitle;
+        }
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        }else{
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        if (town == null || town.isBlank() || town.isEmpty()) {
+            this.town = "Информация не указана";
+        }else {
+            this.town = town;
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ public class Main {
         Human maksim = new Human("Максим", "Минск", "бренд-менеджер",1988);
         Human anya = new Human("Аня", "Москва","методист образовательных программ" ,1993);
         Human katya = new Human("Катя", "Калининград","продакт-менеджер", 0);
-        Human artem = new Human("Артем", "Москва",null, 1995);
+        Human artem = new Human("Артем", "Москва","директор", 1995);
+        Human vladimir = new Human("Владимир", "Казань",null, 1995);
         System.out.println(maksim);
         System.out.println(anya);
         System.out.println(katya);
         System.out.println(artem);
+        System.out.println(vladimir);
+
+        Flower rosa = new Flower("Роза",null, "Голландия", 35.59);
+        Flower hrizantema = new Flower("Хризантема",null, null, 15.00, 5);
+        Flower pion = new Flower("Пион",null, "Англия", 69.9, 1);
+        Flower hipsofila = new Flower("Гипсофила", null, "Турция", 19.5, 10);
+        System.out.println(rosa);
     }
+
 }
